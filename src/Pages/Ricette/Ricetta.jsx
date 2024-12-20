f935e3b78fb4d359fdfbd19f8727e1a1527b723a
micheleDeca <mikideca2003@gmail.com>
2024-12-20 12:41:19 +0100

    state ricetta

--- src/Pages/Ricette/Ricetta.jsx
+++ src/Pages/Ricette/Ricetta.jsx
@@ -6,11 +6,51 @@ import Ingredients from "../../Components/Ingredients/Ingredients.jsx";
 
 function Ricetta() {
 
+    const [ricettaInfo, setRicettaInfo] = useState({
+        nome: "Risotto ai Funghi Porcini Bio",
+        foto: "url_della_foto.jpg",
+        descrizione: "Un cremoso risotto preparato con funghi porcini freschi e ingredienti biologici, ideale per una cena raffinata e salutare.",
+        categorie: ["Bio", "Senza lattosio", "Vegan", "Senza glutine", "Km0", "Vegetariano"],
+        badgeInformativi: {
+            difficolta: "1",
+            tempoPreparazione: "4",
+            tempoCottura: "3",
+            costo: "2",
+            porzioni: "6",
+        },
+        Ingredienti: [
+            { Nome: "Riso Carnaroli bio", Quantita: "320g" },
+            { Nome: "Funghi porcini freschi bio", Quantita: "400g" },
+            { Nome: "Brodo vegetale bio", Quantita: "1,5L" },
+            { Nome: "Cipolla bio", Quantita: "1 media" },
+            { Nome: "Olio extravergine d'oliva bio", Quantita: "3 cucchiai" },
+            { Nome: "Vino bianco bio", Quantita: "100ml" },
+            { Nome: "Parmigiano Reggiano bio", Quantita: "50g" },
+            { Nome: "Prezzemolo fresco bio", Quantita: "q.b." }
+        ],
+        Passaggi: [
+            {
+                Titolo: "Preparare i funghi porcini",
+                Descrizione: "Pulire accuratamente i funghi porcini con un panno umido per rimuovere ogni residuo di terra e tagliarli a fette sottili.",
+                Foto: "risotto_funghi/1.png"
+            },
+            {
+                Titolo: "Soffriggere la cipolla",
+                Descrizione: "Tritare finemente la cipolla e soffriggerla a fuoco medio in una padella con olio extravergine d'oliva fino a doratura leggera.",
+                Foto: "risotto_funghi/2.png"
+            },
+            {
+                Titolo: "Cuocere i funghi",
+                Descrizione: "Aggiungere i funghi porcini nella padella con la cipolla e cuocere per circa 5 minuti, mescolando di tanto in tanto.",
+                Foto: "risotto_funghi/3.png"
+            },
+        ],
+    });
     return (
         <>
             <div className={`popup-edit${modify === "" ? "" : "-active"}`  /*AGGIUNTO*/}>
                 <PopUpModify modify={modify} setModify={setModify} negozioInfo={negozioInfo}
-                             setNegozioInfo={setNegozioInfo}/>
+                    setNegozioInfo={setNegozioInfo} />
             </div>
             <div className="boxRicetta">
                 <ShopWindow
@@ -23,12 +63,12 @@ function Ricetta() {
                 />
                 <span>&nbsp;</span>
                 <RecipesInformation
-                    difficoltà={}
-                    preparazione={}
-                    cottura={}
-                    costo={}/>
+                    difficoltà={ }
+                    preparazione={ }
+                    cottura={ }
+                    costo={ } />
                 <span>&nbsp;</span>
-                <Ingredients boh={}/>
+                <Ingredients boh={ } />
             </div>
         </>
     );
