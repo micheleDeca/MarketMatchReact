39bde9650cfafd6a0aad2d0582e2b7b4bea17b5a
Tails <anbot16@gmail.com>
2024-12-21 14:32:39 +0100

    Recovery

--- src/Pages/Prodotto/Prodotto.jsx
+++ src/Pages/Prodotto/Prodotto.jsx
@@ -46,11 +46,15 @@ function Prodotto(props) {
               originalPrice={prodottoInfo.prezzo.base + "€"}
               modify={setModify}
               badges={prodottoInfo.categorie}
+              mode={props.mode}
            />
           <Caratteristiche peso={"Venduto in: " + prodottoInfo.caratteristiche.PesoDimensioniUnitaria}
                            quantita={prodottoInfo.caratteristiche.Quantità + " " + prodottoInfo.caratteristiche.PesoDimensioniUnitaria}
                            dimensioni={prodottoInfo.caratteristiche.DescrizioneUnita}
-                           modify={setModify}/>
+                           modify={setModify}
+                           disponibile={prodottoInfo.caratteristiche.disponibile}
+                           tipo={props.mode}/>
+
         </div>
       </>
   );
