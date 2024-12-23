562b52a7578150e9d269651fc57919938585d3b5
isabale03 <isabale03@gmail.com>
2024-12-23 19:02:58 +0100

    VE Punti finished

--- src/Pages/Punti/Punti.jsx
+++ src/Pages/Punti/Punti.jsx
@@ -1,8 +1,11 @@
 import "./Punti.css";
 import GreenPointsCO2 from "../../Components/GreenPointsCO2/GreenPointsCO2.jsx";
 import OperationLongContainer from "../../Components/OperationLongContainer/OperationLongContainer.jsx";
+import { useEffect, useState } from "react";
+import ButtonSucessivo from "../../Components/ButtonSucessivo/ButtonSucessivo.jsx";
+import ButtonPrecedente from "../../Components/ButtonPrecedente/ButtonPrecedente.jsx";
 
-const operations = [];
+const mockOperations = [];
 
 const startDate = new Date(2024, 11, 15); // Months are 0-indexed in JavaScript
 
@@ -14,11 +17,29 @@ for (let i = 0; i < 50; i++) {
         pointType: Math.random() > 0.5 ? "plus" : "minus",
     };
 
-    operations.push(operation);
+    mockOperations.push(operation);
 }
 
 function Punti() {
     const punti = 1080;
+
+    const [operations, setOperations] = useState([]); 
+    const [currentPage, setCurrentPage] = useState(1); 
+    const operationsPerPage = 5; 
+
+    const fetchOperations = (page) => {
+        // Recupera le prenotazioni per la pagina attuale
+        const startIndex = (page - 1) * operationsPerPage;
+        const endIndex = startIndex + operationsPerPage;
+        const pageOperations = mockOperations.slice(startIndex, endIndex);
+
+        setOperations(pageOperations);
+    };
+
+
+    useEffect(() => {
+        fetchOperations(currentPage);
+    }, [currentPage]);
 
     return (
         <>
@@ -28,6 +49,16 @@ function Punti() {
             <div className="point-operation-container" >
                 <OperationLongContainer operations={operations} type={"point"} />
             </div>
+            <div className="prenButton">
+                {currentPage >= 2 && (
+                    <ButtonPrecedente onclick={() => setCurrentPage(currentPage - 1)} />
+                )}
+
+                {currentPage < (mockOperations.length / operationsPerPage) && (
+                    <ButtonSucessivo name="Successivo" onclick={() => setCurrentPage(currentPage + 1)} />
+                )}
+            </div>
+
         </>
     );
 }
