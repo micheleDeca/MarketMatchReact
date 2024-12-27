f8e5a566f82fe91f815fad9b76c19dc43d491357
micheleDeca <mikideca2003@gmail.com>
2024-12-27 13:42:06 +0100

    -

--- src/config.js
+++ src/config.js
@@ -1,4 +1,3 @@
-//export const BASE_URL = 'http://localhost:1337';
-export const BASE_URL = 'http://4.232.65.20:1337';
+export const BASE_URL = 'http://localhost:1337';
 
 export const IS_MOCKKED = false;
--- vite.config.js
+++ vite.config.js
@@ -7,12 +7,6 @@ export default defineConfig({
   server: {
     port: 3000, // Specifica la porta
     host: true, // Rende il server accessibile da altri dispositivi
-    proxy: {
-      '/api': { // Sostituisci con il prefisso che usi per le chiamate al backend
-        target: 'http://localhost:1337', // URL del tuo server Strapi
-        changeOrigin: true, // Cambia l'origine per evitare problemi di CORS
-        rewrite: (path) => path.replace(/^\/api/, ''), // Rimuovi il prefisso /api, se necessario
-      },
-    },
+    
   },
 });
