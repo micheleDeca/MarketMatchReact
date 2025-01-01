b56c7345c4f0719e4efdb735b409a1d9142fd58c
Tails <anbot16@gmail.com>
2025-01-01 12:12:24 +0100

    Update Prodotto.jsx

--- src/Pages/Prodotto/Prodotto.jsx
+++ src/Pages/Prodotto/Prodotto.jsx
@@ -10,68 +10,76 @@ import { useState, useEffect } from "react";
 import { getToken } from "../../LocalStorage/TokenStorage.jsx";
 import { useUserContext } from "../../Context/UserContext";
 import { useLocation } from "react-router-dom";
+import LoadingPage from "../LoadingPage/LoadingPage.jsx";
 
 function Prodotto() {
   const { userType } = useUserContext();
   const [modify, setModify] = useState("");
-  
   const location = useLocation();
-    const { id } = location.state || {}; // Fallback se `state` è null
-    console.log("ID", id);
-  /*
-  const [prodottoInfo, setProdottoInfo] = useState({
-    "Quantità": {
-        "low": 50,
-        "high": 0
-    },
-    "DescrizioneUnita": "Venduti in pacchetti da 200g",
-    "Descrizione": "Biscotti croccanti biologici senza glutine.",
-    "Categorie": [
-        "Bio",
-        "Senza Glutine"
-    ],
-    "Nome": "Biscotti Senza Glutine Bio",
-    "Foto": "/prodotto/10.jpg",
-    "Disponibile": true,
-    "DistanzaKm": 1.5536619549214765,
-    "PesoDimensioniUnitaria": "pacchetto",
-    "NomeNegozio": "Bio e Vegan Delights"
-});
-*/
+  const { id } = location.state || {}; // Fallback se `state` è null
   const [prodottoInfo, setProdottoInfo] = useState([]);
   const token = getToken();
+  const [loading, setLoading] = useState(true); // Stato per il caricamento
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     console.log("ID", id);
-    const getProduct = async () => {
-      try {
-        const response = await axios.post(
-          `${BASE_URL}/api/product/getProduct`,
-          {
-            productId: id,
-            userLatitude: 41.1090642,
-            userLongitude: 16.8719847,
-          },
-          {
-            headers: {
-              Authorization: `Bearer ${token}`, // Token di autenticazione
+    if (!IS_MOCKKED) {
+      const getProduct = async () => {
+        try {
+          const response = await axios.post(
+            `${BASE_URL}/api/product/getProduct`,
+            {
+              productId: id,
+              userLatitude: 41.1090642,
+              userLongitude: 16.8719847,
             },
-          }
-        );
-        setProdottoInfo(response.data);
-      } catch (error) {
-        console.error("Errore durante il recupero dei prodotti:", error);
-        throw error;
-      }
-    };
-
-    getProduct();
+            {
+              headers: {
+                Authorization: `Bearer ${token}`, // Token di autenticazione
+              },
+            }
+          );
+          setProdottoInfo(response.data);
+          setLoading(false);
+        } catch (error) {
+          setError(error.message); // Gestisci l'errore
+          setLoading(false);
+        }
+      };
+      getProduct();
+    } else {
+      setProdottoInfo({
+        Quantità: 50,
+        DescrizioneUnita: "Venduti in pacchetti da 200g",
+        Descrizione: "Biscotti croccanti biologici senza glutine.",
+        Categorie: ["Bio", "Senza Glutine"],
+        Nome: "Biscotti Senza Glutine Bio",
+        Foto: "/prodotto/10.jpg",
+        Disponibile: true,
+        DistanzaKm: 1.5536619549214765,
+        Prezzo: "3.20",
+        PrezzoOfferta: "2.00",
+        PesoDimensioniUnitaria: "pacchetto",
+        NomeNegozio: "Bio e Vegan Delights",
+      });
+      setLoading(false);
+    }
   }, []); //inserire prossimamente, aggiornamento in base ai filtri scelti
   /*
   useEffect(() => {
     console.log("AAAA",prodottoInfo);
        },[prodottoInfo]);
   */
+  if (loading)
+    return (
+      <div>
+        <LoadingPage />
+      </div>
+    );
+
+  if (error) return <div>Errore: {error}</div>;
+
   return (
     <>
       <div
