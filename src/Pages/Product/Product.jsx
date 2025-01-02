37d416a15b3562b22dd8d924f0bc3769845405bf
isabale03 <isabale03@gmail.com>
2025-01-02 21:09:55 +0100

    Update Product.jsx

--- src/Pages/Product/Product.jsx
+++ src/Pages/Product/Product.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import ProductContainer from '../../Components/CardContainer/CardContainer';
 import './Product.css';
 import { Pagination } from 'react-pagination-bar';
