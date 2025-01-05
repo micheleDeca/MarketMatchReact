b099ac04f4d6d08883295cf55d0adc4ec08aff1a
isabale03 <isabale03@gmail.com>
2025-01-05 19:15:46 +0100

    -

--- src/Components/Counter/Counter.jsx
+++ src/Components/Counter/Counter.jsx
@@ -2,13 +2,51 @@ import "./Counter.css";
 import {useState, useEffect} from 'react';
 import Add from "./add.png";
 import Sub from "./sub.png";
+import { getToken } from "../../LocalStorage/TokenStorage";
+import { useUserContext } from "../../Context/UserContext";
+import LoadingPage from "../../Pages/LoadingPage/LoadingPage";
 
-function Counter({initialQuantity, getCounter}) {
+
+function Counter({productId, initialQuantity, getCounter}) {
+    const { databaseKey } = useUserContext();
+    const [loading, setLoading] = useState(true); // Stato per il caricamento
+    const [error, setError] = useState(null); // Stato per gli errori
 
     const [contatore, setContatore] = useState(0);
     const incrementa = () => setContatore(contatore + 1);
     const decrementa = () => setContatore(contatore > 0 ? contatore - 1 : 0);
 
+    //Funzione per aggiornare la quantità nel db
+    const updateQuantity = async () => {
+        const token = getToken();
+
+        if (!token || !databaseKey) {
+            throw new Error("Token o databaseKey mancanti");
+        }
+
+        try {
+            const response = await axios.post(
+                `${BASE_URL}/api/cart/setQuantity`,
+                { idUser: databaseKey,
+                    idProduct: productId,
+                    newQuantity: contatore
+                 }, // Corpo della richiesta
+                {
+                    headers: {
+                        Authorization: `Bearer ${token}`, // Token di autenticazione
+                    },
+                }
+            );
+
+            const {idProduct, newQuantity} = response.data;
+
+            // Ritorna un oggetto con stores e products
+            return newQuantity;
+        } catch (error) {
+            console.error("Errore durante il recupero dei prodotti:", error);
+            throw error; // Propaga l'errore
+        }
+    };
     
     useEffect(() =>{
         setContatore(initialQuantity);
@@ -16,8 +54,37 @@ function Counter({initialQuantity, getCounter}) {
 
      useEffect(() =>{
         getCounter(contatore);
+        
+        let isMounted = true; // Flag per evitare aggiornamenti su componenti smontati
+        const modifyQuantity = async () => {
+            try {
+                const { idProduct, newQuantity } = await updateQuantity(); // Usa la funzione per l'aggiornamento della quantità
+                console.log("idProduct", idProduct);
+                console.log("newQuantity",newQuantity);
+                if (isMounted) {
+                    setLoading(false); // Ferma il caricamento
+                }
+            } catch (err) {
+                if (isMounted) {
+                    setError(err.message); // Gestisci l'errore
+                    setLoading(false);
+                }
+            }
+        };
+
+        modifyQuantity();
+
+        // Cleanup: evita aggiornamenti su componenti smontati
+        return () => {
+            isMounted = false;
+        };
+
+
      }, [contatore]);
      
+
+    if (loading) return <div><LoadingPage /></div>;
+    if (error) return <div>Errore: {error}</div>;
 
     return (
         <>
--- src/Components/PrenotazioneCarrello/PrenotazioneCarrello.jsx
+++ src/Components/PrenotazioneCarrello/PrenotazioneCarrello.jsx
@@ -6,29 +6,30 @@ import './PrenotazioneCarrello.css'
 
 export default function PrenotazioneCarrello(value) {
 
-    const getCounter = (aa) =>{
+    const getCounter = (aa) => {
         console.log(aa);
     }
-    
+
     return (
         <>
             <div className="prenotationBox">
                 <div className="contentBox">
                     <div className="productList">
-                        <CardLongList title={value.nameNeg} products={value.products} getCounter={getCounter} type={"product"}/>
+                        <CardLongList title={value.nameNeg} products={value.products.map((product)=>(product.product))}
+                         getCounter={getCounter} type={"product"} />
                     </div>
 
                     <div className="dateBox">
-                        <LuogoDataRitiro nameNeg={value.nameNeg} provincia={value.luogoDataInfo.province} citta={value.luogoDataInfo.city} 
-                        cap={value.luogoDataInfo.cap} indirizzo={value.luogoDataInfo.address}
-                        contatti={value.luogoDataInfo.cell}
-                        orari={value.luogoDataInfo.storeHours} />
+                        <LuogoDataRitiro nameNeg={value.nameNeg} provincia={value.luogoDataInfo.province} citta={value.luogoDataInfo.city}
+                            cap={value.luogoDataInfo.cap} indirizzo={value.luogoDataInfo.address}
+                            contatti={value.luogoDataInfo.cell}
+                            orari={value.luogoDataInfo.storeHours} />
                     </div>
                 </div>
                 <div className="totalBox">
-                    <h1>TOTALE: { value.luogoDataInfo.totalPrice } €</h1>
+                    <h1>TOTALE: {value.luogoDataInfo.totalPrice} €</h1>
                     <div className="prenoteButton">
-                        <ButtonSave name={"PRENOTA ordine " + value.nameNeg}/>
+                        <ButtonSave name={"PRENOTA ordine"} />
                     </div>
                 </div>
             </div>
